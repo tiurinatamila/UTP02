010b79b51e67f50831c9f3ab62fc9d593e19e25e
AleksejVolodko <s30829@pjwstk.edu.pl>
2024-10-08 16:40:01 +0200

    Subtractor class file

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
         Adder adder = new Adder();
         System.out.println(adder.add(1, 2));
         Subtractor subtractor = new Subtractor();
-        System.out.println(subtractor.subtract(6, 3));
+        System.out.println(subtractor.subtract(10, 2));
     }
 }
