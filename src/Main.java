bb1574e495b6665d711e91189a2618da72cf6d8f
tiurinatamila <s30844@pjwstk.edu.pl>
2024-10-08 16:41:27 +0200

    Change values in add

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
+        System.out.println(adder.add(11, 4));
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(10, 2));
     }
